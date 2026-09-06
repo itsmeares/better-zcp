105850b6a8fc40d90e3764e23123eff3532aa3ee
Aras <72440556+itsmeares@users.noreply.github.com>
2026-09-06 18:59:40 +0100

    chore: add TanStack Query and server typecheck gate

--- apps/panel-client/src/main.tsx
+++ apps/panel-client/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import './i18n'
 import { isDemoMode, installDemoFetchShim } from './lib/demo'
 import { BuildCompatibilityGate } from './components/BuildCompatibilityGate'
+import { queryClient } from './lib/queryClient'
 
 const Router = isDemoMode() ? HashRouter : BrowserRouter
 
@@ -18,10 +20,12 @@ if (isDemoMode()) {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
-      <BuildCompatibilityGate>
-        <App />
-      </BuildCompatibilityGate>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <BuildCompatibilityGate>
+          <App />
+        </BuildCompatibilityGate>
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
