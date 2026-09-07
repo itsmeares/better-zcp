c9f1e66648a9078b887333b001ff03a82fc8e5c5
Aras <72440556+itsmeares@users.noreply.github.com>
2026-09-07 16:16:32 +0100

    fix(panel): use one TanStack Query provider (#18)

--- apps/panel-client/src/main.tsx
+++ apps/panel-client/src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from '@tanstack/react-router'
 import './index.css'
 import './i18n'
 import { isDemoMode, installDemoFetchShim } from './lib/demo'
-import { BuildCompatibilityGate } from './components/BuildCompatibilityGate'
-import { queryClient } from './lib/queryClient'
 import { router } from './router'
 
 if (isDemoMode()) {
@@ -18,10 +15,6 @@ if (isDemoMode()) {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BuildCompatibilityGate>
-        <RouterProvider router={router} />
-      </BuildCompatibilityGate>
-    </QueryClientProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
